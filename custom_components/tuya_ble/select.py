10bc24a111b6c3bee01a2bf544d5ae20f44f5355
Sebastian Mondial <kappuchino@users.noreply.github.com>
2024-01-19 10:07:02 +0000

    Update select.py with riecov42 values

--- custom_components/tuya_ble/select.py
+++ custom_components/tuya_ble/select.py
@@ -138,7 +138,8 @@ mapping: dict[str, TuyaBLECategorySelectMapping] = {
         products={
             **dict.fromkeys(
                 [
-                    "mknd4lci"
+                    "mknd4lci",
+                    "riecov42"
                 ],  # Fingerbot Plus
                 [
                     TuyaBLEFingerbotModeMapping(dp_id=101),
